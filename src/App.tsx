b436f3094beec02d2ca8361c89bb163a9c49f807
Group02-F24 <abdelmohsen@aucegypt.edu>
2025-02-10 13:33:41 +0200

    Created React Project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Title from './Title';
+
+function App() {
+  return <div><Title /></div>;
+}
+
+export default App
---
+++ src/Title.tsx
@@ -0,0 +1,6 @@
+
+function Title() {
+    return <h1>Mental Wellbeing Assistant</h1>;
+}
+
+export default Title;
